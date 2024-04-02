092fd9c74905f4e41baa3289910d02b0841254dc
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-04-02 22:40:16 +0400

    add recognize of meeting

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/usegetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { Loader } from "lucide-react";
+import React, { useState } from "react";
 
-const Meeeting = ({ params }: { params: { id: string } }) => {
-  return <div>Meeeting Room: #{params.id}</div>;
+const Meeeting = ({ params: { id } }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+  return (
+    <main className=" h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default Meeeting;
