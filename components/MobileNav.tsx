77a3414e3ffb7e6fd12f3c03e19dd6f042db6016
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-04-04 18:49:27 +0400

    add new meeting section

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -37,7 +37,7 @@ const MobileNav = () => {
               alt="Yoom logo"
               className=" max-sm:size-10"
             />
-            <p className=" text-[26px] font-extrabold text-white">Yoom</p>
+            <p className=" text-[26px] font-extrabold text-white">Foom</p>
           </Link>
 
           <div className=" flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
